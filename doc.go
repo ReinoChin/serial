07742e2fea61a63bbb6dba21c4c1cf16659f4f50
ReinoChin <reino.chin@gmail.com>
2018-04-24 15:11:55 +0800

    Create doc.go

---
+++ doc.go
@@ -0,0 +1,38 @@
+// serial project doc.go
+
+/*
+serial document
+
+Example:
+func main() {
+	config := serial.Config{
+		Port:   "/dev/ttyUSB0",
+		Baud:   115200,
+		Bits:   8,
+		Parity: 'N',
+		Stop:   1}
+	uart, err := serial.OpenPort(&config)
+	if err != nil {
+		fmt.Println("open error")
+		return
+	}
+
+	sendLen, _ := uart.Write([]byte("hello"))
+	fmt.Println(sendLen)
+
+	buf := make([]byte, 4096)
+	for {
+		size, err := uart.ReadAggregation(buf, serial.INFINITE, 50*time.Millisecond)
+		if err != nil {
+			fmt.Printf("%s\n", err)
+			break
+		} else {
+			if size > 0 {
+				fmt.Printf("%d-%s\n",size, buf)
+			}
+		}
+	}
+}
+*/
+package serial
+
